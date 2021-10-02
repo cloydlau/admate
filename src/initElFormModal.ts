e333bd87f92021642c0c9ee3c74f1aec4201a292
cloydlau <253055734@qq.com>
2021-10-02 10:14:41 +0800

    :construction:

--- src/initElFormModal.ts
+++ src/initElFormModal.ts
@@ -1,9 +1,9 @@
 export default elFormRef => {
   // 给各筛选项赋初值，使得重置功能能够正常工作
-  if (elFormRef.value.fields) {
+  if (elFormRef.fields) {
     return Object.fromEntries(
       Array.from(
-        elFormRef.value.fields,
+        elFormRef.fields,
         (v: any) => [v.labelFor, undefined]
       )
     )
