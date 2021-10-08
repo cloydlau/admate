e76fed631ed968a57ab3ea8bb28bf3940f48310f
cloydlau <253055734@qq.com>
2021-10-08 11:19:14 +0800

    剔除createAxiosShortcut

--- src/api-generator.ts
+++ src/api-generator.ts
@@ -1,12 +1,7 @@
-import { stringify } from 'qs'
 import { merge } from 'lodash-es'
 import { CancelToken } from 'axios'
-import { jsonToFormData } from 'kayran'
 
-const CONSOLE_PREFIX = import.meta.env.VITE_APP_CONSOLE_PREFIX
 const METHODS_WITH_REQUEST_BODY = ['PUT', 'POST', 'DELETE', 'PATCH']
-const METHODS_WITHOUT_REQUEST_BODY = ['GET', 'HEAD']
-const METHODS = METHODS_WITH_REQUEST_BODY.concat(METHODS_WITHOUT_REQUEST_BODY)
 let source
 
 export type ConfigCatalogType = {
@@ -110,85 +105,6 @@ export default function createAPIGenerator (
 
     return result
   }
-}
-
-export const createAxiosShortcut = (axios: (args: any) => Promise<any>): {
-  'GET'?: (args: any) => Promise<any>,
-  'POST'?: (args: any) => Promise<any>,
-  'DELETE'?: (args: any) => Promise<any>,
-  'PATCH'?: (args: any) => Promise<any>,
-  'HEAD'?: (args: any) => Promise<any>,
-  'PUT'?: (args: any) => Promise<any>,
-  'DOWNLOAD'?: (args: any) => Promise<any>,
-} => {
-  let result = {}
-
-  const download = (url, data, config: {
-    method?: string
-  } = {}) => {
-    if (config.method) {
-      return axios({
-        responseType: 'blob',
-        url,
-        ...METHODS_WITH_REQUEST_BODY.includes(config.method.toUpperCase()) ? { data } : { params: data },
-        ...config
-      })
-    } else {
-      window.open(url + stringify(data, { addQueryPrefix: true }))
-    }
-  }
-
-  (result as any).DOWNLOAD = download
-
-  const upload = (url, data, config) => {
-    return axios({
-      url,
-      method: 'POST',
-      data: jsonToFormData(data),
-      ...config
-    })
-  }
-
-  METHODS.map(v => {
-    const value = (url, data, config = {}) => {
-      return axios({
-        method: v,
-        url,
-        ...METHODS_WITH_REQUEST_BODY.includes(v.toUpperCase()) ? { data } : { params: data },
-        ...config
-      })
-    }
-
-    value.download = function () {
-      // arguments不适用于箭头函数
-      // @ts-ignore
-      const [url, data, config] = arguments
-      if (config?.method) {
-        console.warn(`${CONSOLE_PREFIX}method无法重复指定`)
-      }
-      return download(url, data, {
-        ...config,
-        method: v,
-      })
-    }
-
-    value.upload = function () {
-      // arguments不适用于箭头函数
-      // @ts-ignore
-      const [url, data, config] = arguments
-      if (config?.method) {
-        console.warn(`${CONSOLE_PREFIX}method无法重复指定`)
-      }
-      return upload(url, data, {
-        ...config,
-        method: v,
-      })
-    }
-
-    result[v] = value
-  })
-
-  return result
 }
 
 export const cancelAllRequest = () => {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-import { createAxiosShortcut } from './api-generator'
-import initElFormModal from './initElFormModal'
 import useAdmate from './useAdmate'
 
 export default useAdmate
-export {
-  initElFormModal,
-  createAxiosShortcut,
-}
