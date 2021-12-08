6d7752b92c0eb8dba208f83f27e6ddd63e840cba
cloydlau <253055734@qq.com>
2021-12-08 17:19:41 +0800

    workflow

--- scripts/release.js
+++ scripts/release.js
@@ -4,6 +4,7 @@ const path = require('path')
 const chalk = require('chalk')
 const semver = require('semver')
 const currentVersion = require('../package.json').version
+const pkg = require('../package.json').name
 const { prompt } = require('enquirer')
 const execa = require('execa')
 
@@ -13,11 +14,6 @@ const preId =
 const isDryRun = args.dry
 //const skipTests = args.skipTests
 const skipBuild = args.skipBuild
-/*const packages = fs
-.readdirSync(path.resolve(__dirname, '../packages'))
-.filter(p => !p.endsWith('.ts') && !p.startsWith('.'))*/
-
-const skippedPackages = []
 
 const versionIncrements = [
   'patch',
@@ -33,7 +29,6 @@ const run = (bin, args, opts = {}) =>
 const dryRun = (bin, args, opts = {}) =>
   console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
 const runIfNotDry = isDryRun ? dryRun : run
-//const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg)
 const step = msg => console.log(chalk.cyan(msg))
 
 async function main () {
@@ -85,24 +80,20 @@ async function main () {
     console.log(`(skipped)`)
   }*/
 
-  // update all package versions and inter-dependencies
-  step('\nUpdating cross dependencies...')
-  updateVersions(targetVersion)
-
   // build all packages with types
-  step('\nBuilding all packages...')
+  step('\nBuilding...')
   if (!skipBuild && !isDryRun) {
     await run('pnpm', ['run', 'build'])
     // test generated dts files
-    step('\nVerifying type declarations...')
-    await run('pnpm', ['run', 'test-dts-only'])
+    //step('\nVerifying type declarations...')
+    //await run('pnpm', ['run', 'test-dts-only'])
   } else {
     console.log(`(skipped)`)
   }
 
   // generate changelog
-  step('\nGenerating changelog...')
-  await run(`pnpm`, ['run', 'changelog'])
+  //step('\nGenerating changelog...')
+  //await run(`pnpm`, ['run', 'changelog'])
 
   // update pnpm-lock.yaml
   step('\nUpdating lockfile...')
@@ -118,98 +109,28 @@ async function main () {
   }
 
   // publish packages
-  /*step('\nPublishing packages...')
-  for (const pkg of packages) {
-    await publishPackage(pkg, targetVersion, runIfNotDry)
-  }*/
+  step('\nPublishing...')
+  await publishPackage(pkg, targetVersion, runIfNotDry)
 
   // push to GitHub
   step('\nPushing to GitHub...')
-  //await runIfNotDry('git', ['tag', `v${targetVersion}`])
-  //await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
-  //await runIfNotDry('git', ['push'])
+  await runIfNotDry('git', ['tag', `v${targetVersion}`])
+  await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
+  await runIfNotDry('git', ['push'])
 
   if (isDryRun) {
     console.log(`\nDry run finished - run git diff to see package changes.`)
   }
 
-  if (skippedPackages.length) {
-    console.log(
-      chalk.yellow(
-        `The following packages are skipped and NOT published:\n- ${skippedPackages.join(
-          '\n- '
-        )}`
-      )
-    )
-  }
   console.log()
 }
 
-function updateVersions (version) {
-  // 1. update root package.json
-  updatePackage(path.resolve(__dirname, '..'), version)
-  // 2. update all packages
-  //packages.forEach(p => updatePackage(getPkgRoot(p), version))
-}
-
-function updatePackage (pkgRoot, version) {
-  const pkgPath = path.resolve(pkgRoot, 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
-  pkg.version = version
-  //updateDeps(pkg, 'dependencies', version)
-  //updateDeps(pkg, 'peerDependencies', version)
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
-}
-
-function updateDeps (pkg, depType, version) {
-  const deps = pkg[depType]
-  if (!deps) return
-  Object.keys(deps).forEach(dep => {
-    if (
-      dep === 'vue'
-      //|| (dep.startsWith('@vue') && packages.includes(dep.replace(/^@vue\//, '')))
-    ) {
-      console.log(
-        chalk.yellow(`${pkg.name} -> ${depType} -> ${dep}@${version}`)
-      )
-      deps[dep] = version
-    }
-  })
-}
-
 async function publishPackage (pkgName, version, runIfNotDry) {
-  if (skippedPackages.includes(pkgName)) {
-    return
-  }
-  /*const pkgRoot = getPkgRoot(pkgName)
-  const pkgPath = path.resolve(pkgRoot, 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
-  if (pkg.private) {
-    return
-  }*/
-
-  // For now, all 3.x packages except "vue" can be published as
-  // `latest`, whereas "vue" will be published under the "next" tag.
-  let releaseTag = null
-  if (args.tag) {
-    releaseTag = args.tag
-  } else if (version.includes('alpha')) {
-    releaseTag = 'alpha'
-  } else if (version.includes('beta')) {
-    releaseTag = 'beta'
-  } else if (version.includes('rc')) {
-    releaseTag = 'rc'
-  } else if (pkgName === 'vue') {
-    // TODO remove when 3.x becomes default
-    releaseTag = 'next'
-  }
-
-  // TODO use inferred release channel after official 3.0 release
-  // const releaseTag = semver.prerelease(version)[0] || null
+  const releaseTag = semver.prerelease(version)[0] || null
 
   step(`Publishing ${pkgName}...`)
   try {
-    /*await runIfNotDry(
+    await runIfNotDry(
       // note: use of yarn is intentional here as we rely on its publishing
       // behavior.
       'yarn',
@@ -222,10 +143,10 @@ async function publishPackage (pkgName, version, runIfNotDry) {
         'public'
       ],
       {
-        cwd: pkgRoot,
+        //cwd: pkgRoot,
         stdio: 'pipe'
       }
-    )*/
+    )
     console.log(chalk.green(`Successfully published ${pkgName}@${version}`))
   } catch (e) {
     if (e.stderr.match(/previously published/)) {
