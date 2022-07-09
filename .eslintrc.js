ccf389dc2a957b30da7c0cef62d6d84914538ba3
cloydlau <253055734@qq.com>
2022-07-10 00:01:16 +0800

    workflow: docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
+/**
+ * 搭建 eslint + prettier：
+ *   1. pnpm add lint-staged eslint eslint-plugin-vue @typescript-eslint/eslint-plugin @typescript-eslint/parser prettier eslint-config-prettier eslint-plugin-prettier -D
+ *   2. npx husky add .husky/pre-commit "npx lint-staged"
+ */
+
 module.exports = {
   env: {
     browser: true,
