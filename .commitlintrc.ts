2eeca3d01306c6b4406dfb189214e0a415551993
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-01-17 14:00:34 +0800

    workflow: modify commitlint rules

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -4,6 +4,7 @@ const { rules } = require('@commitlint/config-conventional')
 // See https://github.com/vuejs/core/blob/main/scripts/verifyCommit.js
 rules['type-enum'][2].push('wip', 'types', 'release', 'workflow', 'dx')
 rules['header-max-length'][2] = 200
+rules['subject-case'][0] = 0
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
