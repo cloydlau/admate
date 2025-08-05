92711e94903fca13f2dfdebb307e7930d9db5a1f
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2025-08-05 17:03:23 +0800

    release: v1.0.0-rc.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,28 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+import nounsanitized from 'eslint-plugin-no-unsanitized'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
 
 export default antfu(
   {
     formatters: true,
     lessOpinionated: true,
+    unocss: true,
   },
   {
     rules: {
       'brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
       'curly': ['error', 'all'],
-      'no-console': 'off',
+      'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
       'vue/max-attributes-per-line': ['error', { singleline: 3 }],
       'vue/max-len': ['error', {
         code: 160,
@@ -34,4 +47,14 @@ export default antfu(
       },
     },
   },
+  [nounsanitized.configs.recommended], // Code Security Check
+  /**
+   * mimic ESLintRC-style extends
+   *
+   * This rule is used to avoid financial calculations
+   * For non-financial calculation cases, you can add the comments below to ignore:
+   *   // Reason for ignoring: Not related to finance, calculating xxx
+   *   // eslint-disable-next-line financial/no-division (or financial/no-float-calculation)
+   */
+  ...compat.extends('plugin:financial/recommended'),
 )
